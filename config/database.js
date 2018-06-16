b05c83f603f0efdba226464125e3faf0e7ae105e
Jay Wang <jaywang007@yahoo.com>
2018-06-15 22:31:09 -0500

    database config

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+  // running mongodb locally in docker mapps to port 37017
+  // I will show it to you about running mongodb on docker
+  database: 'mongodb://localhost:37017/wheelsandbyes',
+  secret: itsyourbirthday
+}
